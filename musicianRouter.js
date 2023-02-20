b8bdeac5c32e5f95669290c1418ec7edc6f1b367
Luke Page <kelly.luke2705@gmail.com>
2023-02-20 12:16:29 +0000

    Completed Code

---
+++ musicianRouter.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const { Musician } = require('./Musician');
+
+const musicianRouter = express.Router();
+
+// CREATE
+musicianRouter.post('/musicians', async (req, res) => {
+  try {
+    const musician = await Musician.create(req.body);
+    res.status(201).json(musician);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// READ
+musicianRouter.get('/musicians', async (req, res) => {
+  try {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// UPDATE
+musicianRouter.put('/musicians/:id', async (req, res) => {
+  try {
+    const [ rowsUpdated, [ updatedMusician ] ] = await Musician.update(req.body, {
+      where: { id: req.params.id },
+      returning: true
+    });
+    if (rowsUpdated === 0) {
+      res.status(404).send('Not Found');
+    } else {
+      res.json(updatedMusician);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// DELETE
+musicianRouter.delete('/musicians/:id', async (req, res) => {
+  try {
+    const rowsDeleted = await Musician.destroy({
+      where: { id: req.params.id }
+    });
+    if (rowsDeleted === 0) {
+      res.status(404).send('Not Found');
+    } else {
+      res.status(204).end();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = musicianRouter;
--- server.js
+++ server.js
@@ -5,7 +5,34 @@ const {sequelize} = require("./db")
 
 const port = 3000;
 
+app.use(express.json());
+
+
 //TODO
+app.get("/musicians", async (req, res) => {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+  });
+
+app.get('/musicians/:id', async (req, res) => {
+  const musician = await Musician.findByPk(req.params.id);
+  if (musician) {
+    res.json(musician);
+  } else {
+    res.status(404).json({ message: "Musician not found" });
+  }
+});
+
+app.post('/musicians', async (req, res) => {
+    try {
+      const musician = await Musician.create(req.body);
+      res.status(201).json(musician);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+  
+app.use(musicianRouter);
 
 app.listen(port, () => {
     sequelize.sync();
